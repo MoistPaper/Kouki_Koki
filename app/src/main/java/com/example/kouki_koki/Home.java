a514c7309df4036fc4b897419c618fe75b8d3f56
blooge4231 <57263072+blooge4231@users.noreply.github.com>
2021-05-25 11:44:22 -0400

    Update Home.java

--- app/src/main/java/com/example/kouki_koki/Home.java
+++ app/src/main/java/com/example/kouki_koki/Home.java
@@ -1,6 +1,4 @@
-/*
-This activity is created for the user to pick how many time intervals he/she wants
- */
+
 package com.example.kouki_koki;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +11,6 @@ import android.widget.NumberPicker;
 import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
-    //Instantiate widgets and variables
     private NumberPicker numberPicker;
     private TextView textView;
     private int session = 1;
@@ -23,10 +20,8 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         textView= (TextView) findViewById(R.id.selected);
         numberPicker = (NumberPicker) findViewById(R.id.numberPicker);
-        //Set font
         Typeface quicksand=Typeface.createFromAsset(getAssets(),"fonts/Quicksand-Regular.ttf");
         textView.setTypeface(quicksand);
-        //Set value for numberpickers
         numberPicker.setMaxValue(5);
         numberPicker.setMinValue(1);
         numberPicker.setValue(1);
@@ -39,7 +34,6 @@ public class Home extends AppCompatActivity {
             }
         });
     }
-    //Go to next activity
     public void start(View v){
         Intent intent = new Intent(this, Interval.class);
         intent.putExtra("session",session);
